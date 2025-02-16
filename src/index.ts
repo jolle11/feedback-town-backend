465ade0fd41c633b58c9b135ea8e2b3c852b3688
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2025-02-16 15:49:37 +0100

    feat: add clerk authentication

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import type { AddressInfo } from "node:net";
 import Fastify from "fastify";
 import dotenv from "dotenv";
+import { clerkPlugin, clerkClient, getAuth } from "@clerk/fastify";
+import "dotenv/config";
 
 dotenv.config();
 
@@ -19,8 +21,30 @@ const server = Fastify({
 			: true,
 });
 
+server.register(clerkPlugin, {
+	publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+	secretKey: process.env.CLERK_SECRET_KEY,
+});
+
 server.get("/", (req, reply) => {
 	reply.send({ hello: "wasup!" });
+});
+
+server.get("/protected", async (req, reply) => {
+	try {
+		const { userId } = getAuth(req);
+
+		if (!userId) {
+			return reply.code(401).send({ error: "User not found" });
+		}
+
+		const user = userId ? await clerkClient.users.getUser(userId) : null;
+
+		return reply.send({ message: "User authenticated successfully", user });
+	} catch (err) {
+		server.log.error(err);
+		return reply.code(500).send({ error: "Failed to authenticate user" });
+	}
 });
 
 const start = async () => {
